80caf4b0b19709d0d0453e42d5b24955552020fc
joseph muli <jayjaymuli00@gmail.com>
2016-08-06 14:31:03 +0300

    problem solving

---
+++ SaturdayThe6th/datasource.md
@@ -0,0 +1,6 @@
+## Problem Solving Skills  
+> The tasks below are meant to teach and guide students on problem solving skills  
+
+### Standards:  
+1. Pseudocode for each problem  
+2. Code commenting
---
+++ SaturdayThe6th/list_sum.py
@@ -0,0 +1,22 @@
+"""
+Create a function that gets the sum of all the digits in a list given as a parameter
+
+"""
+
+
+def sum_digits(a):
+	'''
+		Takes a list A
+			-Returns the sum of all the digits in a list
+	'''
+	# Code goes here:
+
+
+
+"""
+Test cases include:
+
+sum_digits([10,30,45])
+	----This should return 13
+
+"""
