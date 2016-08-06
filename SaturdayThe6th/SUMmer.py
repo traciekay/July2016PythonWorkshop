68ce72edacf46affd5b8027975426d02a3a24519
joseph muli <jayjaymuli00@gmail.com>
2016-08-06 14:34:54 +0300

    SUMmer

---
+++ SaturdayThe6th/SUMmer.py
@@ -0,0 +1,15 @@
+"""
+Create a function that:
+	1. Halves even numbers
+	2. Doubles odd numbers
+	3. Returns the sum of all
+"""
+
+def super_sum(num):
+	# Your awesome code flows from here
+
+
+
+
+
+
