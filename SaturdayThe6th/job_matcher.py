6c91a16c1319b413541490370a774291a5a0cce3
joseph muli <jayjaymuli00@gmail.com>
2016-08-06 15:38:36 +0300

    evened dictionary pairs

--- SaturdayThe6th/job_matcher.py
+++ SaturdayThe6th/job_matcher.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
 Given 50 randomly located drivers, map them to some random number of randomly located jobs such that
-total distance to jobs is minimised. With 100 jobs available, map the driver ids and job location ids
+total distance to jobs is minimised. With only 50 jobs available, map the driver ids and job location ids
 and print out the result in a key value pair
 """
